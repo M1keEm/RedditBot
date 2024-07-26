7f696c83dc9c1266f483e1984f4ff31ba6864d95
MikeEm <michalbanaszek03@gmail.com>
2024-07-26 17:54:06 +0200

    optimized imports

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
+#!/usr/bin/python
+import os
+import re
+
 import praw
 
+# Create the Reddit instance
 reddit = praw.Reddit('bot1')
 
-subreddit = reddit.subreddit("learnpython")
+# and login
+# reddit.login(REDDIT_USERNAME, REDDIT_PASS)
 
-# to verify whether the instance is authorized instance or not
-print(reddit.read_only)
+# Have we run this code before? If not, create an empty list
+if not os.path.isfile("posts_replied_to.txt"):
+    posts_replied_to = []
 
-# subreddit = reddit.subreddit('GRE')
+# If we have run the code before, load the list of posts we have replied to
+else:
+    # Read the file into a list and remove any empty values
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = list(filter(None, posts_replied_to))
 
-# display the subreddit name
-print(subreddit.display_name)
+# Get the top 5 values from our subreddit
+subreddit = reddit.subreddit('pythonforengineers')
+for submission in subreddit.hot(limit=10):
+    # print(submission.title)
 
-# display the subreddit title
-print(subreddit.title)
+    # If we haven't replied to this post before
+    if submission.id not in posts_replied_to:
 
-# display the subreddit description
-print(subreddit.description)
+        # Do a case insensitive search
+        if re.search("i love python", submission.title, re.IGNORECASE):
+            # Reply to the post
+            submission.reply("Nigerian scammer bot says: It's all about the Bass (and Python)")
+            print("Bot replying to : ", submission.title)
+
+            # Store the current id into our list
+            posts_replied_to.append(submission.id)
+
+# Write our updated list back to the file
+with open("posts_replied_to.txt", "w") as f:
+    for post_id in posts_replied_to:
+        f.write(post_id + "\n")
